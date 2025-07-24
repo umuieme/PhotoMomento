b31aab14d7e434c0653078218154431139e61e5d
Umesh Basnet <basnet0umesh@gmail.com>
2025-07-24 15:18:29 -0400

    Initial Commit

---
+++ PhotoMomento/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoMomento
+//
+//  Created by Umesh Basnet on 2025-07-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoMomento/PhotoMomentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMomentoApp.swift
+//  PhotoMomento
+//
+//  Created by Umesh Basnet on 2025-07-24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMomentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
